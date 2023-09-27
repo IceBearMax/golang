1f276626968c76a3aa0ee1bc49c0f6d94a0f356e
IceBearMax <1034089857@qq.com>
2023-09-27 17:06:16 +0800

    golang 表单 初级

---
+++ web-go/loginfunc.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"crypto/md5"
+	"fmt"
+	"html/template"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method)
+	if r.Method == "GET" {
+		timestamp := strconv.Itoa(time.Now().Nanosecond())
+		hashWr := md5.New()
+		hashWr.Write([]byte(timestamp))
+		token := fmt.Sprintf("%x", hashWr.Sum(nil))
+
+		t, _ := template.ParseFiles("login.gtpl")
+		t.Execute(w, token)
+	} else {
+		r.ParseForm()
+		token := r.Form.Get("token")
+		if token != "" {
+
+		} else {
+
+		}
+		fmt.Println("username length", len(r.Form["username"][0]))
+		fmt.Println("username:", template.HTMLEscapeString(r.Form.Get("username")))
+		fmt.Println("password:", template.HTMLEscapeString(r.Form.Get("password")))
+		template.HTMLEscape(w, []byte(r.Form.Get("username")))
+
+	}
+
+}
---
+++ web-go/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm() //解析url传递参数
+	fmt.Println(r.Form)
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for k, v := range r.Form {
+		fmt.Println("key:", k)
+		fmt.Println("val:", strings.Join(v, ""))
+		fmt.Println(w, "hello astaxie!")
+	}
+
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("methon:", r.Method)
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("login.gtpl")
+		log.Println(t.Execute(w, nil))
+	} else {
+		fmt.Println("username:", r.Form["username"])
+		fmt.Println("password:", r.Form["password"])
+	}
+}
+
+func main() {
+	http.HandleFunc("/", sayhelloName)
+	http.HandleFunc("/login", login)
+	err := http.ListenAndServe(":9090", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
